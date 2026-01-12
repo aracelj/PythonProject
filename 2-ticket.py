b81fab17ea1f5fa4fe75a1fc5a9c3dec2e5e3d2d
aracelj <aracelj10@gmail.com>
2026-01-12 20:00:51 +0100

    Comment.

--- 2-ticket.py
+++ 2-ticket.py
@@ -1,9 +1,11 @@
-# Calculates the change for a ticket from the cash
-# Calculate the amount that every person pays
+# Räkna ut hur mycket som blir kvar, efter att man betalat en biljett
+# Pengarna som blir över ska dela med en vän
 
 biljet = 100  # biljettpris
 kontant = 200  # pengar på fickan
 print ("Det blir" ,(kontant - biljet),  "kronor över.")
 change = (kontant - biljet )/ 2
 print("Varje person får", change)
+
+
 
