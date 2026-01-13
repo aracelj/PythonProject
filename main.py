788f25a196821ee14280e48c0dd8e025cf15b2ad
aracelj <aracelj10@gmail.com>
2026-01-13 14:13:27 +0100

    Comment.

---
+++ main.py
@@ -0,0 +1,12 @@
+# Uppgift 1. This program prints a message to the screen
+# Hello World
+# This program was made by Araceli Jakobsson #
+
+print ("Hello World \nThis program was made by Araceli Jakobsson")
+
+
+
+
+
+
+
---
+++ u2.py
@@ -0,0 +1,19 @@
+"""
+Uppgift #2
+Räkna ut hur mycket som blir kvar, efter att man betalat en biljett
+Pengarna som blir över ska dela med en vän
+ursprung kod och ändrar till rätt koder
+     x = 100  # biljettpris
+     y = 200  # pengar på fickan
+     print("Det blir " + (y - x) " kronor över.")
+     z = y - x / 2
+     print("Varje person får " + z)
+"""
+
+biljet = 100  # biljettpris
+kontant = 200  # pengar på fickan
+print ("\nBiljet: 100 \nKontant:200 kronor")
+print ("Det blir" ,(kontant - biljet),  "kronor över.")
+change = (kontant - biljet )/ 2
+print("Varje person får", change,"kronor")
+
