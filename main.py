aeca09a00ba0d03d566208911af95d970bef92a5
aracelj <aracelj10@gmail.com>
2026-01-13 14:16:57 +0100

    Comment.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Uppgift 1. This program prints a message to the screen
 # Hello World
-# This program was made by Araceli Jakobsson #
+# This program was made by Araceli Jakobsson
 
 print ("Hello World \nThis program was made by Araceli Jakobsson")
 
