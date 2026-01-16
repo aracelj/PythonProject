9df366de2d580dff88be81e3b554981a91d86ff8
aracelj <aracelj10@gmail.com>
2026-01-16 17:26:56 +0100

    Comment.

--- U4_2_triangle.py
+++ U4_2_triangle.py
@@ -4,5 +4,5 @@
 import math
 b_base = float(input("\nEnter measurement of the base of a triangle: "))
 a_height  = float(input("Enter measurement of the height of a triangle: "))
-hypotenuse = math.sqrt(b_base**2 + a_height**2)  # computes the hypotenuse using the pythagorean theorem c² = a² + b_base²
-print ("The hypotenuse of a triangle using the pythagorean theorem:", round(hypotenuse,2))
+c_hypotenuse = math.sqrt(b_base**2 + a_height**2)  # computes the hypotenuse using the pythagorean theorem c² = a² + b_base²
+print ("The hypotenuse of a triangle using the pythagorean theorem:", round(c_hypotenuse,2))
