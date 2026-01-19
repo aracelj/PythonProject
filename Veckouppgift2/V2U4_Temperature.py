0b5ed76edf34dd7761bd05b5b7cb87206e2adc68
aracelj <aracelj10@gmail.com>
2026-01-20 00:33:58 +0100

    Comment.

--- Veckouppgift2/V2U4_Temperature.py
+++ Veckouppgift2/V2U4_Temperature.py
@@ -11,8 +11,8 @@
 # Version 1 Converts the temperature in celsius to fahrenheit
 print("======= Version 1 Converts temperature from celsius to fahrenheit =======")
 input_celsius = float(input("Enter temperature in degrees celsius: "))
-conversion_celsius_to_fahrenheit = input_celsius * 1.8 + 32
-print("It will be", round(conversion_celsius_to_fahrenheit,3),"degrees Fahrenheit.")
+conversion_celsius_to_fahrenheit = 1.8 * input_celsius + 32
+print("It will be", round(conversion_celsius_to_fahrenheit,1),"degrees Fahrenheit.")     # converts from celsius to fahrenheit
 
 
 # Version 2 Ask user whether to use celsius or fahrenheit and converts respectively
@@ -20,21 +20,21 @@ input_temperature = 0
 input_celsius = 0
 print("\n======= Version 2 Converts temperature from celsius to fahrenheit or vice versa =======")
 input_temperature = int(input("Celsius to Fahnrenheit (1) or Fahrenheit to Celsius (2), Enter 1 or 2: "))
-if input_temperature == 1:
+if input_temperature == 1:                                                          # converts from celsius to fahrenheit
       input_celsius = float(input("Enter temperature in degrees celsius: "))
-      conversion_to_fahrenheit = (input_celsius * 1.8) + 32
+      conversion_to_fahrenheit = 1.8 * input_celsius + 32
       final_conversion = conversion_to_fahrenheit
       input_temperature = "Fahrenheit"
-      print("It will be", round(final_conversion, 3), "degrees", input_temperature, ".")
+      print("It will be", round(final_conversion,1), "degrees", input_temperature, ".")
 
-elif input_temperature == 2:
+elif input_temperature == 2:                                                       # converts from fahrenheit to celsius
       input_fahrenheit = float(input("Enter temperature in degrees fahrenheit: "))
       conversion_to_celsius = (input_fahrenheit - 32) / 1.8
       final_conversion = conversion_to_celsius
       input_temperature = "Celsius"
-      print("It will be", round(final_conversion, 3), "degrees", input_temperature, ".")
+      print("It will be", round(final_conversion, 2), "degrees", input_temperature, ".")
 
-else:
+else:                                                                               # wrong selection between celsius (1) & fahrenheit (2)
       print("Wrong selection, it should be 1 or 2.")
 
 
