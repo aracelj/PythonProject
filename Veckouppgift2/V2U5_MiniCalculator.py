f3756537f4b0ec22799ca7d0f27b4ba21fd6d8b3
aracelj <aracelj10@gmail.com>
2026-01-20 10:24:12 +0100

    Comment.

--- Veckouppgift2/V2U5_MiniCalculator.py
+++ Veckouppgift2/V2U5_MiniCalculator.py
@@ -1,4 +1,4 @@
-# Uppgift5 Mini Calculator by Araceli Jakobsson
+# Uppgift 5 Mini Calculator by Araceli Jakobsson
 # 1 User is ask to enter 3 numbers and display the sum
 # 2 Determines the highest among the 3 inputted number without using max function
 # 3 Give the counts of all the numbers that are the same
@@ -15,46 +15,100 @@ print("Sum of all 3 numbers: ", round(total,2))
 
 # 2 Determines the highest among the 3 inputted number without using max function
 print("\n============================= Maximum Number ============================")
-max_number = number_1
-middle_number = number_1
+max_number = 0
 counter_same = 0
-
-if max_number < number_2:
+middle_number = 0
+alike_2 = 0
+alike_3 = 0
+if number_1 < number_2 and number_1 < number_3:
     max_number = number_2
-    middle_number = number_2
-    if max_number < number_3:
-        max_number = number_3
-        middle_number = number_2
-
-    elif max_number == number_3:
-        max_number = number_3
-        counter_same +=1
-
-    else:
-        middle_number = number_1
-
-elif max_number > number_2:
-    max_number = number_1
+    alike_2 = "no"
+    alike_3 = "no"
     middle_number = number_1
-    if max_number < number_3:
+    if number_2 < number_3:
         max_number = number_3
         middle_number = number_2
-
-    elif max_number == number_3:
-        max_number = number_3
+        counter_same += 0
+    elif number_2 == number_3:
+        max_number = number_2
+        middle_number = "none"
         counter_same += 2
+        alike_2 = "yes"
+    elif number_2 > number_3:
+        max_number = number_2
+        middle_number = number_3
+        counter_same +=0
 
-    else:
+
+elif number_2 < number_1 and number_2 < number_3:
+    max_number = number_1
+    #counter_same += 0
+    alike_2 = "no"
+    alike_3 = "no"
+    middle_number = number_2
+    if number_1 < number_3:
+        max_number = number_3
+        middle_number = number_1
+        counter_same += 0
+    elif number_1 == number_3:
+        max_number = number_1
+        middle_number = "none"
+        counter_same += 2
+        alike_2 = "yes"
+    elif number_1 > number_3:
+        max_number = number_1
+        middle_number = number_3
+        counter_same += 0
+
+elif number_3 < number_1 and number_3 < number_2:
+    max_number = number_1
+    counter_same += 1
+    alike_2 = "no"
+    alike_3 = "no"
+    middle_number = number_3
+    if number_1 < number_2:
+        max_number = number_2
+        middle_number = number_1
+        counter_same += 0
+    elif number_1 == number_2:
+        max_number = number_1
+        middle_number = "none"
+        counter_same +=2
+        alike_2 = "yes"
+    elif number_1 > number_2:
+        max_number = number_1
         middle_number = number_2
-
-else:
-   counter_same +=2
-   if max_number == number_3:
-       counter_same += 1
-   elif max_number > number_3:
-       counter_same +=0
-   else:
-       counter_same +=0
+        counter_same +=0
+elif number_1 == number_2 and number_1 == number_3:
+    max_number = number_1
+    counter_same += 3
+    alike_2 = "yes"
+    alike_3 = "yes"
+    middle_number = number_1
+elif number_1 == number_2 and number_1 < number_3:
+    max_number = number_3
+    counter_same += 2
+    alike_2 = "yes"
+    alike_3 = "yes"
+    middle_number = "none"
+elif number_1 == number_3 and number_1 < number_2:
+    max_number = number_2
+    counter_same += 2
+    alike_2 = "yes"
+    alike_3 = "no"
+    middle_number = "none"
+elif number_2 == number_3 and number_2 < number_1:
+    max_number = number_1
+    counter_same += 2
+    alike_2 = "yes"
+    alike_3 = "no"
+    middle_number = "none"
+elif number_2 == number_3 and number_2 > number_3:
+    max_number = number_3
+    counter_same += 2
+    alike_2 = "yes"
+    alike_3 = "no"
+    middle_number = "none"
 print("The maximum number is", max_number)
 
 
@@ -64,20 +118,11 @@ print("Count of numbers alike: ", counter_same)
 
 
 # 4 Displays the middle number only if all three numbers are different or all are the same numbers.
-if counter_same == 2:
-    alike_2 = "yes"
-    alike_3 = "no"
-    middle_number = "none"
-elif counter_same == 3:
-    alike_2 = "yes"
-    alike_3 = "yes"
-else:
-    alike_2 = "no"
-    alike_3 = "no"
+print("middle no.: ", middle_number)
 
 print("\n================================ 5.4 Table ===============================")
 print("__________________________________________________________________________")
 print("| 1st No. | 2nd No. | 3rd No.| Maximum | 2 Alike? | 3 Alike? | Middle No. |")
-print("  " , number_1,"    ", number_2,"     ", number_3, "     ", max_number,"    ", alike_2,"       ", alike_3,"       ", middle_number,"    ")
+print(" " , number_1,"     ", number_2,"     ", number_3, "     ", max_number,"    ", alike_2,"      ", alike_3,"       ", middle_number,"    ")
 print("|_________________________________________________________________________|")
 
