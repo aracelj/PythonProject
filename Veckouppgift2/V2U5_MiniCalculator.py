c14ac08e34b3c8fd5e3452c08a144581f4462c40
aracelj <aracelj10@gmail.com>
2026-01-20 10:56:27 +0100

    Comment.

--- Veckouppgift2/V2U5_MiniCalculator.py
+++ Veckouppgift2/V2U5_MiniCalculator.py
@@ -2,7 +2,8 @@
 # 1 User is ask to enter 3 numbers and display the sum
 # 2 Determines the highest among the 3 inputted number without using max function
 # 3 Give the counts of all the numbers that are the same
-# 4 Displays the middle number only if all three numbers are different or all are the same. Otherwise it will not count it as middle no.
+# 4 Displays the middle number only if all three numbers are different or all are the same. Otherwise it will not count it as middle no. Display in a table format
+
 
 
 # 1 User is ask to enter 3 numbers and display the sum
@@ -20,95 +21,90 @@ counter_same = 0
 middle_number = 0
 alike_2 = 0
 alike_3 = 0
-if number_1 < number_2 and number_1 < number_3:
+if number_1 < number_2 and number_1 < number_3:          #checks if number_1 is less than number_2 and number_3
     max_number = number_2
     alike_2 = "no"
     alike_3 = "no"
     middle_number = number_1
-    if number_2 < number_3:
+    if number_2 < number_3:                              #checks if number_3 is the maximum number
         max_number = number_3
         middle_number = number_2
         counter_same += 0
-    elif number_2 == number_3:
+    elif number_2 == number_3:                           #checks if number_2 and number_3 are equal
         max_number = number_2
         middle_number = "none"
         counter_same += 2
         alike_2 = "yes"
-    elif number_2 > number_3:
+    elif number_2 > number_3:                            #checks if number_2 is the maximum number
         max_number = number_2
         middle_number = number_3
         counter_same +=0
 
 
-elif number_2 < number_1 and number_2 < number_3:
+elif number_2 < number_1 and number_2 < number_3:        #checks if number_2 is less than number_1 and number_3
     max_number = number_1
     #counter_same += 0
     alike_2 = "no"
     alike_3 = "no"
     middle_number = number_2
-    if number_1 < number_3:
+    if number_1 < number_3:                              #checks if number_3 is the maximum number
         max_number = number_3
         middle_number = number_1
         counter_same += 0
-    elif number_1 == number_3:
+    elif number_1 == number_3:                           #checks if number_1 and number_3 are equal
         max_number = number_1
         middle_number = "none"
         counter_same += 2
         alike_2 = "yes"
-    elif number_1 > number_3:
+    elif number_1 > number_3:                            #checks if number_1 is the maximum number
         max_number = number_1
         middle_number = number_3
         counter_same += 0
 
-elif number_3 < number_1 and number_3 < number_2:
+elif number_3 < number_1 and number_3 < number_2:        #checks if number_3 is less than number_1 and number_2
     max_number = number_1
     counter_same += 1
     alike_2 = "no"
     alike_3 = "no"
     middle_number = number_3
-    if number_1 < number_2:
+    if number_1 < number_2:                              #checks if number_2 is the maximum number
         max_number = number_2
         middle_number = number_1
         counter_same += 0
-    elif number_1 == number_2:
+    elif number_1 == number_2:                           #checks if number_1 and number_2 are equal
         max_number = number_1
         middle_number = "none"
         counter_same +=2
         alike_2 = "yes"
-    elif number_1 > number_2:
+    elif number_1 > number_2:                            #checks if number_1 is the maximum number
         max_number = number_1
         middle_number = number_2
         counter_same +=0
-elif number_1 == number_2 and number_1 == number_3:
+elif number_1 == number_2 and number_1 == number_3:      #checks if all the 3 numbers are equal
     max_number = number_1
     counter_same += 3
     alike_2 = "yes"
     alike_3 = "yes"
     middle_number = number_1
-elif number_1 == number_2 and number_1 < number_3:
+elif number_1 == number_2 and number_1 < number_3:        #checks if number_3 is the maximum number
     max_number = number_3
-    counter_same += 2
-    alike_2 = "yes"
-    alike_3 = "yes"
-    middle_number = "none"
-elif number_1 == number_3 and number_1 < number_2:
-    max_number = number_2
     counter_same += 2
     alike_2 = "yes"
     alike_3 = "no"
     middle_number = "none"
-elif number_2 == number_3 and number_2 < number_1:
+elif number_2 == number_3 and number_2 < number_1:        #checks if number_1 is the maximum number
     max_number = number_1
     counter_same += 2
     alike_2 = "yes"
     alike_3 = "no"
     middle_number = "none"
-elif number_2 == number_3 and number_2 > number_3:
-    max_number = number_3
+elif number_1 == number_3 and number_1 < number_2:        #checks if number_2 is the maximum number
+    max_number = number_2
     counter_same += 2
     alike_2 = "yes"
     alike_3 = "no"
     middle_number = "none"
+
 print("The maximum number is", max_number)
 
 
@@ -117,9 +113,7 @@ print("\n============================= Numbers Alike ===========================
 print("Count of numbers alike: ", counter_same)
 
 
-# 4 Displays the middle number only if all three numbers are different or all are the same numbers.
-print("middle no.: ", middle_number)
-
+# 4 Displays the middle number only if all three numbers are different or all are the same numbers in a table format
 print("\n================================ 5.4 Table ===============================")
 print("__________________________________________________________________________")
 print("| 1st No. | 2nd No. | 3rd No.| Maximum | 2 Alike? | 3 Alike? | Middle No. |")
