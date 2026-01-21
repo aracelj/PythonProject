718bf03f4dc25e0606fa52724059a75228db8f07
aracelj <aracelj10@gmail.com>
2026-01-21 12:27:36 +0100

    \\\

--- Veckouppgift2/V2U5_MiniCalculator.py
+++ Veckouppgift2/V2U5_MiniCalculator.py
@@ -21,85 +21,66 @@ counter_same = 0
 middle_number = 0
 alike_2 = 0
 alike_3 = 0
-if number_1 < number_2 and number_1 < number_3:          #checks if number_1 is less than number_2 and number_3
+alike_2 = "no"
+alike_3 = "no"
+if number_1 < number_2 and number_1 < number_3:          #checks if number_1 is the lowest number
     max_number = number_2
-    alike_2 = "no"
-    alike_3 = "no"
     middle_number = number_1
-    if number_2 < number_3:                              #checks if number_3 is the maximum number
+    if number_2 < number_3:                              #checks if number_3 > number_2 while number_1 is the lowest number
         max_number = number_3
         middle_number = number_2
-        counter_same += 0
-    elif number_2 == number_3:                           #checks if number_2 and number_3 are equal
+    elif number_2 == number_3:                           #checks if number_2 == number_3 while number_1 is the lowest number
         max_number = number_2
         middle_number = "none"
         counter_same += 2
         alike_2 = "yes"
-    elif number_2 > number_3:                            #checks if number_2 is the maximum number
+    elif number_2 > number_3:                            #checks if number_2 > number_3 while number_1 is the lowest number
         max_number = number_2
         middle_number = number_3
-        counter_same +=0
 
 
-elif number_2 < number_1 and number_2 < number_3:        #checks if number_2 is less than number_1 and number_3
+elif number_2 < number_1 and number_2 < number_3:        #checks if number_2 is the lowest number
     max_number = number_1
-    #counter_same += 0
-    alike_2 = "no"
-    alike_3 = "no"
     middle_number = number_2
-    if number_1 < number_3:                              #checks if number_3 is the maximum number
+    if number_1 < number_3:                              #checks if number_3 > number_1 while number_2 is the lowest number
         max_number = number_3
         middle_number = number_1
-        counter_same += 0
-    elif number_1 == number_3:                           #checks if number_1 and number_3 are equal
+
+    elif number_1 == number_3:                           #checks if number_1 == number_3 while number_2 is the lowest number
         max_number = number_1
         middle_number = "none"
         counter_same += 2
         alike_2 = "yes"
-    elif number_1 > number_3:                            #checks if number_1 is the maximum number
+
+    elif number_1 > number_3:                            #checks if number_1 > number_3 while number_2 is the lowest number
         max_number = number_1
         middle_number = number_3
-        counter_same += 0
 
-elif number_3 < number_1 and number_3 < number_2:        #checks if number_3 is less than number_1 and number_2
+
+elif number_3 < number_1 and number_3 < number_2:        #checks if number_3 is the lowest number
     max_number = number_1
-    counter_same += 1
-    alike_2 = "no"
-    alike_3 = "no"
     middle_number = number_3
-    if number_1 < number_2:                              #checks if number_2 is the maximum number
+    if number_1 < number_2:                              #checks if number_2 > number_1 while number_3 is the lowest number
         max_number = number_2
         middle_number = number_1
-        counter_same += 0
-    elif number_1 == number_2:                           #checks if number_1 and number_2 are equal
+
+    elif number_1 == number_2:                           #checks if number_1 == number_2 while number_3 is the lowest number
         max_number = number_1
         middle_number = "none"
         counter_same +=2
         alike_2 = "yes"
-    elif number_1 > number_2:                            #checks if number_1 is the maximum number
+
+    elif number_1 > number_2:                            #checks if number_1 > number_2 while number_3 is the lowest number
         max_number = number_1
         middle_number = number_2
-        counter_same +=0
+
 elif number_1 == number_2 and number_1 == number_3:      #checks if all the 3 numbers are equal
     max_number = number_1
     counter_same += 3
     alike_2 = "yes"
     alike_3 = "yes"
     middle_number = number_1
-elif number_1 == number_2 and number_1 < number_3:        #checks if number_3 is the maximum number
-    max_number = number_3
-    counter_same += 2
-    alike_2 = "yes"
-    alike_3 = "no"
-    middle_number = "none"
-elif number_2 == number_3 and number_2 < number_1:        #checks if number_1 is the maximum number
-    max_number = number_1
-    counter_same += 2
-    alike_2 = "yes"
-    alike_3 = "no"
-    middle_number = "none"
-elif number_1 == number_3 and number_1 < number_2:        #checks if number_2 is the maximum number
-    max_number = number_2
+else:                                                    #none of the above conditions are true then it takes that it has two numbers alike
     counter_same += 2
     alike_2 = "yes"
     alike_3 = "no"
@@ -114,9 +95,18 @@ print("Count of numbers alike: ", counter_same)
 
 
 # 4 Displays the middle number only if all three numbers are different or all are the same numbers in a table format
-print("\n================================ 5.4 Table ===============================")
-print("__________________________________________________________________________")
-print("| 1st No. | 2nd No. | 3rd No.| Maximum | 2 Alike? | 3 Alike? | Middle No. |")
-print(" " , number_1,"     ", number_2,"     ", number_3, "     ", max_number,"    ", alike_2,"      ", alike_3,"        ", middle_number,"    ")
-print("|_________________________________________________________________________|")
+print("\n================================== 5.4 Table ===================================")
+print(" ________________________________________________________________________________")
+print("| 1st No. | 2nd No. | 3rd No. |  Maximum  |  2 Alike?  |  3 Alike?  | Middle No. |")
+row = "|" + "  " + str(number_1) + "      " + str(number_2) + "       " + str(number_3) + "        " + str(max_number) + "        " + alike_2 + "           " + alike_3 + "          " + str(middle_number)
+row = row.ljust(81) + "|"                                                              #prints "|" on a fixed column position
+print(row)
+print("|________________________________________________________________________________|")
 
+#import shutil
+
+#size = shutil.get_terminal_size()
+#print("Terminal width:", size.columns)
+#print("Terminal height:", size.lines)
+
+#print("\033[2;1H" + "|                                                                                |", end="")
