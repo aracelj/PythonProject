13612339f705535a7cb0f8140e0137ce6c27e072
aracelj <aracelj10@gmail.com>
2026-01-28 11:02:08 +0100

    Comment.

---
+++ Veckouppgift 3/1_1_While_loop.py
@@ -0,0 +1,27 @@
+# Exercise 1_1 While loop by Araceli Jakobsson
+# What is the output of the while loop
+"""
+Output of the code:
+limit = 15
+index = 5
+while index <= limit:
+   print(index)
+   index = index + 2
+
+Output will be:
+5
+7
+9
+11
+13
+15
+"""
+
+
+# 1 Output of the While loop
+
+limit = 15
+index = 5
+while index <= limit:
+   print(index)
+   index = index + 2
